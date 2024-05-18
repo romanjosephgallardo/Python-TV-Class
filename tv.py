e913633238bead676311de9dfcb857bd34598a16
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-18 21:57:51 +0800

    Added pseudocode

---
+++ tv.py
@@ -0,0 +1,19 @@
+# pseudocode
+
+# create TV class
+#   create method for default TV object
+#   create method for turning on TV
+#   create method for turning off TV
+#   create method that returns the channel
+#   create method that sets new channel
+#   create method that gets the volume level
+#   create method that sets new volume
+#   create method that increases channel number by 1
+#   create method that decreases channel number by 1
+#   create method that increases volume level by 1
+#   create method that decreases volume level by 1
+
+# create two TV objects
+# set channel and volume for tv1
+# set channel and volume for tv2
+# Print the channel and volume level of the two TV  OBJECT
