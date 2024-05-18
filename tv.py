dbb0a2851ce45bd1b0ef33f349827d0c0d5737ed
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-18 23:25:16 +0800

    Created a two TV objects, set the channels individually, and print them for testing the output

--- tv.py
+++ tv.py
@@ -23,6 +23,8 @@ class TV:
     def set_channel(self, channel):
         if channel in range(1, 121):
             self.channel = channel
+
+
 #   create method that gets the volume level
 #   create method that sets new volume
 #   create method that increases channel number by 1
@@ -31,6 +33,14 @@ class TV:
 #   create method that decreases volume level by 1
 
 # create two TV objects
+tv_1 = TV()
+tv_2 = TV()
+
 # set channel and volume for tv1
+tv_1.set_channel(2)
+
 # set channel and volume for tv2
+tv_2.set_channel(64)
 # Print the channel and volume level of the two TV  OBJECT
+print(f"tv1's channel is {tv_1.get_channel()}")
+print(f"tv2's channel is {tv_2.get_channel()}")
