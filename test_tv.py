e496ccf00e6c88521f8c1f830320c8cf00d4fa91
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-19 15:48:10 +0800

    Added a TV selection for TV 1 to do specific actions

--- test_tv.py
+++ test_tv.py
@@ -17,7 +17,33 @@ while True:
           " 3. Show the two TV status \n"
           " 4. Exit the program")
 
-# Print the channel and volume level of the two TV OBJECT
+    tv_choice = input("Enter your option: ")
+
+    if tv_choice == '1':
+        selected_tv = tv_1
+        tv_name = "TV 1"
+        print(f"\nControlling the {tv_name}. Choose an action: \n"
+              " 1. Increase Channel \n"
+              " 2. Decrease Channel \n"
+              " 3. Increase Volume \n"
+              " 4. Decrease Volume \n"
+              " 5. Back to TV Selection")
+
+        action_choice = input("Enter your choice (1-5): ")
+
+        # Performing the action
+        if action_choice == '1':
+            tv_1.channel_up()
+        if action_choice == '2':
+            tv_2.channel_down()
+        if action_choice == '3':
+            tv_1.volume_up()
+        if action_choice == '4':
+            tv_2.volume_down()
+        if action_choice == '5':
+            continue
+
+    # Print the channel and volume level of the two TV OBJECT
     print(f"tv1's channel is {tv_1.get_channel()} and volume level is {tv_1.get_volume()}")
     print(f"tv2's channel is {tv_2.get_channel()} and volume level is {tv_2.get_volume()}")
-    break
+    continue
