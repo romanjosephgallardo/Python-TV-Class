dcfe19b5be596ed1cc1d6ff2870d14b8118d92c3
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-19 15:23:23 +0800

    Implemented a while True loop for interaction with user of the program

--- test_tv.py
+++ test_tv.py
@@ -8,14 +8,16 @@ tv_2 = TV()
 tv_1.turn_on()
 tv_2.turn_on()
 
-# set channel and volume for tv1
-tv_1.set_channel(120)
-tv_1.set_volume(7)
-
-# set channel and volume for tv2
-tv_2.set_channel(64)
-tv_2.set_volume(7)
+# Interaction with user
+while True:
+    # Displays options to user
+    print("Choose a TV to control! \n"
+          " 1. TV 1 \n"
+          " 2. TV 2 \n"
+          " 3. Show the two TV status \n"
+          " 4. Exit the program")
 
 # Print the channel and volume level of the two TV OBJECT
-print(f"tv1's channel is {tv_1.get_channel()} and volume level is {tv_1.get_volume()}")
-print(f"tv2's channel is {tv_2.get_channel()} and volume level is {tv_2.get_volume()}")
+    print(f"tv1's channel is {tv_1.get_channel()} and volume level is {tv_1.get_volume()}")
+    print(f"tv2's channel is {tv_2.get_channel()} and volume level is {tv_2.get_volume()}")
+    break
