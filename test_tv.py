f022df8a3e9a8cb7858ec4dfca222d5a7d399d20
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-05-19 16:07:59 +0800

    Refactor TV control logic to handle multiple TVs with shared actions

--- test_tv.py
+++ test_tv.py
@@ -22,51 +22,11 @@ while True:
     if tv_choice == '1':
         selected_tv = tv_1
         tv_name = "TV 1"
-        print(f"\nControlling the {tv_name}. Choose an action: \n"
-              " 1. Increase Channel \n"
-              " 2. Decrease Channel \n"
-              " 3. Increase Volume \n"
-              " 4. Decrease Volume \n"
-              " 5. Back to TV Selection")
-
-        action_choice = input("Enter your choice (1-5): ")
-
-        # Performing the action
-        if action_choice == '1':
-            tv_1.channel_up()
-        if action_choice == '2':
-            tv_1.channel_down()
-        if action_choice == '3':
-            tv_1.volume_up()
-        if action_choice == '4':
-            tv_1.volume_down()
-        if action_choice == '5':
-            continue
 
     # For TV 2
     if tv_choice == '2':
         selected_tv = tv_2
         tv_name = "TV 2"
-        print(f"\nControlling the {tv_name}. Choose an action: \n"
-              " 1. Increase Channel \n"
-              " 2. Decrease Channel \n"
-              " 3. Increase Volume \n"
-              " 4. Decrease Volume \n"
-              " 5. Back to TV Selection")
-
-        action_choice = input("Enter your choice (1-5): ")
-
-        # Performing the action
-        if action_choice == '1':
-            tv_2.channel_up()
-        if action_choice == '2':
-            tv_2.channel_down()
-        if action_choice == '3':
-            tv_2.volume_up()
-        if action_choice == '4':
-            tv_2.volume_down()
-        if action_choice == '5':
-            continue
 
     if tv_choice == '3':
         print(f"tv1's channel is {tv_1.get_channel()} and volume level is {tv_1.get_volume()}")
@@ -76,6 +36,27 @@ while True:
     if tv_choice == '4':
         print("Thank you for using the program!")
         break
+
+    print(f"\nControlling the {tv_name}. Choose an action: \n"
+          " 1. Increase Channel \n"
+          " 2. Decrease Channel \n"
+          " 3. Increase Volume \n"
+          " 4. Decrease Volume \n"
+          " 5. Back to TV Selection")
+
+    action_choice = input("Enter your choice (1-5): ")
+
+    # Performing the action
+    if action_choice == '1':
+        selected_tv.channel_up()
+    if action_choice == '2':
+        selected_tv.channel_down()
+    if action_choice == '3':
+        selected_tv.volume_up()
+    if action_choice == '4':
+        selected_tv.volume_down()
+    if action_choice == '5':
+        continue
 
     # Print the channel and volume level of the two TV OBJECT
     print(f"tv1's channel is {tv_1.get_channel()} and volume level is {tv_1.get_volume()}")
